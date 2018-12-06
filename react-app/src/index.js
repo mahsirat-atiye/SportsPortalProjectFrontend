06b2af2c4fe55e2adbd2295db2e133e8fa42fc82
mahsirat-atiye <atieh.atieh.jamshidpoor@gmail.com>
2018-12-06 12:45:45 +0330

    index.js changed to check components

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import Togglingnews from './components/Togglingnews'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import Listoftogglingnews from './components/Listoftogglingnews'
+// import Chraracteristicplayer from './components/Chraracteristicplayer'
+// import Listofplayers from './components/Listofplayers'
+
+//import Footballplayerpage from './components/Footballplayerpage'
+//import Footballstatistics from './components/Footballstatistics'
+//import Basketballplayerpage from './components/Basketballplayerpage'
+//import Timeline_ from './components/Timeline_'
+import Videoplayer from './components/Videoplayer'
+ReactDOM.render(<Videoplayer />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
