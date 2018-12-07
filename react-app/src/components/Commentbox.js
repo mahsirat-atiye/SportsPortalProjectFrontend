2ee95ba9412ddf17a93800980ab5ee2e61a3ad69
mahsirat-atiye <atieh.atieh.jamshidpoor@gmail.com>
2018-12-07 16:43:16 +0330

    comment box added to news

--- react-app/src/components/Commentbox.js
+++ react-app/src/components/Commentbox.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {FormGroup,ControlLabel,FormControl,HelpBlock} from "react-bootstrap";
+import {FormGroup,ControlLabel,FormControl,HelpBlock,Button} from "react-bootstrap";
 
 class Commentbox extends Component {
     constructor(props, context) {
@@ -27,20 +27,28 @@ class Commentbox extends Component {
     render() {
         return (
             <form>
-                <FormGroup
-                    controlId="formBasicText"
-                    validationState={this.getValidationState()}
-                >
-                    <ControlLabel>Working example with validation</ControlLabel>
-                    <FormControl
-                        type="text"
-                        value={this.state.value}
-                        placeholder="Enter text"
-                        onChange={this.handleChange}
-                    />
-                    <FormControl.Feedback />
-                    <HelpBlock>Validation is based on string length.</HelpBlock>
-                </FormGroup>
+                <br/>
+                <br/>
+                <label htmlFor="email"><b>لطفا نظرتان را راجع به اخبار ارائه فرمایید.</b></label>
+                <input type="text" placeholder="متن را وارد کنید." name="comments" style={{
+                    width:'100%',
+                    height:'200px',
+                    padding: '15px',
+                    margin: '5px 0 22px 0',
+                    display: 'inline-block',
+                    border: 'none',
+                    background: '#f1f1f1'
+                }}/>
+                <button style={{
+                    background: '#4CAF50',
+                    color: 'white',
+                    padding: '14px 20px',
+                    margin: '8px 0',
+                    border: 'none',
+                    cursor: 'pointer',
+                    width: '100%',
+                    opacity: '0.9'
+                }}>ارائه نظر</button>
             </form>
         );
     }
