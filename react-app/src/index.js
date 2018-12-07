618123079b9714e032d5081c7cc2a5120d2d6e49
Rev <alinajim@comp.nus.edu.sg>
2018-12-07 20:44:49 +0330

    bug removal

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
-import "font-awesome/css/font-awesome.min.css";
-import "bootstrap-css-only/css/bootstrap.min.css";
-import "mdbreact/dist/css/mdb.css";
+// import "font-awesome/css/font-awesome.min.css";
+// import "bootstrap-css-only/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "mdbreact/dist/css/mdb.css";
 // import Togglingnews from './components/Togglingnews'
 
 // import Listoftogglingnews from './components/Listoftogglingnews'
