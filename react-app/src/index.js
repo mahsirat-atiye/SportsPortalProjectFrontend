f77d5d78c0e3a1be9851b15087a5e4a3190abcaa
mahsirat-atiye <atieh.atieh.jamshidpoor@gmail.com>
2018-12-07 16:02:08 +0330

    comment box added

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
+import "font-awesome/css/font-awesome.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 // import Togglingnews from './components/Togglingnews'
 
 // import Listoftogglingnews from './components/Listoftogglingnews'
@@ -20,8 +23,10 @@ import * as serviceWorker from './serviceWorker';
 //import Saved_and_main_players_table_football from './components/Saved_and_main_players_table_football'
 //import Basketballgamepage from './components/Basketballgamepage'
 //import Newspage_mainpart from './components/Newspage_mainpart'
-import Newspage from './components/Newspage'
-ReactDOM.render(<Newspage/>, document.getElementById('root'));
+//import Newspage from './components/Newspage'
+//import Searchbox from './components/Searchbox'
+import Commentbox from './components/Commentbox'
+ReactDOM.render(<Commentbox/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
