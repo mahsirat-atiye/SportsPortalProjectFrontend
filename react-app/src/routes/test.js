12f245c23823231deb30fc359006079a780dc380
Najimi <alini_cs@yahoo.com>
2018-12-10 14:48:31 +0330

    updates

---
+++ react-app/src/routes/test.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Box from './news/Box2';
+import Demo2 from './tools/panel_group.js';
+import Demo from './table/retable.js';
+
+export default class test extends Component {
+    render() {
+        return (
+            <div>
+                <Demo2/>
+            </div>
+        );
+    }
+}
